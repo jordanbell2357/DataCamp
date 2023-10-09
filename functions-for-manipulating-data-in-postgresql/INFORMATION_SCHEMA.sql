e019635e3227494e88c717a90c940e1e37a0a10a
Jordan Bell <jordan.bell@gmail.com>
2023-10-09 12:12:55 -0400

    datacamp

--- functions-for-manipulating-data-in-postgresql/INFORMATION_SCHEMA.sql
+++
@@ -1,8 +0,0 @@
--- Get the column name and data type
-SELECT
- 	column_name, 
-    data_type
--- From the system database information schema
-FROM INFORMATION_SCHEMA.COLUMNS 
--- For the customer table
-WHERE table_name = 'customer';
